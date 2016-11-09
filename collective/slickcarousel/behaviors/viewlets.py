4966bb1cfc4b0e5cac6aa89ead439fefa2db4710
andreesg <andreslb1@gmail.com>
2016-11-09 12:48:11 +0100

    Add support for videos based on Link content type. Interface enhancements.

--- collective/slickcarousel/behaviors/viewlets.py
+++ collective/slickcarousel/behaviors/viewlets.py
@@ -16,9 +16,38 @@ class SlickCarouselViewlet(ViewletBase):
             return True
         return False
 
+    def generate_slide_item_from_brain(self, brain):
+        item = {}
+
+        path = brain.getPath()
+        url = brain.getURL()
+        title = brain.Title
+        description = brain.Description
+        slide_id = brain.getId
+        item_portal_type = brain.portal_type
+
+        if item_portal_type == "Link":
+            url = brain.getRemoteUrl
+
+        item = {
+            "type": item_portal_type,
+            "url": url,
+            "path": path,
+            "title": title,
+            "description": description,
+            "id": slide_id
+        }
+
+        return item
+
     def get_items(self):
         result = []
         # Check if slideshow exists
+        # Check if folder
+        # Check if collection
+
+        #print self.context.getPhysicalPath()
+
         if self.context.get('slideshow', None):
             # Get items inside of slideshow
             slideshow = self.context['slideshow']
@@ -28,9 +57,14 @@ class SlickCarouselViewlet(ViewletBase):
             for brain in items:
                 if getattr(brain, 'leadMedia', None) and brain.portal_type != "Image":
                     img = uuidToCatalogBrain(brain.leadMedia)
-                    result.append({"url": img.getURL()})
+                    slide_item = self.generate_slide_item_from_brain(brain)
+                    result.append(slide_item)
+                elif brain.portal_type == "Link":
+                    slide_item = self.generate_slide_item_from_brain(brain)
+                    result.append(slide_item)
                 elif brain.portal_type == "Image":
-                    result.append({"url": brain.getURL()})
+                    slide_item = self.generate_slide_item_from_brain(brain)
+                    result.append(slide_item)
             return result
         else:
             return result
--- collective/slickcarousel/resources/js/slickcarousel.js
+++ collective/slickcarousel/resources/js/slickcarousel.js
@@ -1,14 +1,124 @@
-
 /* JS ENHANCEMENTS */
 
 require(['jquery', 'slick-carousel-dist'], 
 function(jQuery, slickDist) {
-	jQuery(document).ready(function() {
-		jQuery(".slideshow-wrapper").slick({
-			infinite: true,
-			slidesToShow: 1,
-			dots: false,
-			speed: 500
-		});
-	});
+
+    var slickCarousel = {};
+    
+    slickCarousel.updateHash = function(slick, currentSlide) {
+        // Get current slide
+        var $currslide = jQuery(slick.$slides[currentSlide]);
+        
+        // Get slide data-path
+        var path = $currslide.data('path');
+
+        // Add hash to url
+        var original_path = window.location.href.split(/\?|#/)[0];
+        var absolute_path = original_path + "#" + path;
+        history.replaceState(null, null, absolute_path);
+    };
+
+    slickCarousel.initialPosition = function() {
+        jQuery(slickCarousel.elem).removeClass("backward");
+        jQuery(slickCarousel.elem).removeClass("forward");
+
+        if (!jQuery(slickCarousel.elem).hasClass("initial-slide")) {
+            jQuery(slickCarousel.elem).addClass("initial-slide");
+        }
+    };
+
+    slickCarousel.moveForward = function() {
+        jQuery(slickCarousel.elem).removeClass("initial-slide");
+        jQuery(slickCarousel.elem).removeClass("backward");
+        jQuery(slickCarousel.elem).removeClass("forward");
+
+        if (!jQuery(slickCarousel.elem).hasClass("forward")) {
+            jQuery(slickCarousel.elem).addClass("forward");
+        }
+    };
+
+    slickCarousel.moveBackwards = function() {
+        jQuery(slickCarousel.elem).removeClass("initial-slide");
+        jQuery(slickCarousel.elem).removeClass("backward");
+        jQuery(slickCarousel.elem).removeClass("forward");
+
+        if (!jQuery(slickCarousel.elem).hasClass("backward")) {
+            jQuery(slickCarousel.elem).addClass("backward");
+        }
+    };
+
+    slickCarousel.changeDirection = function(slick, currentSlide) {
+        var prev = slickCarousel.prevSlide;
+
+        // initial - show both
+        if (currentSlide == 0) {
+            slickCarousel.initialPosition();
+        } else if (prev == 0 && currentSlide == slick.$slides.length-1) {
+            slickCarousel.moveBackwards();
+        } else if (prev < currentSlide) {
+            slickCarousel.moveForward();
+        } else {
+            slickCarousel.moveBackwards();
+        }
+    };
+
+    slickCarousel.afterChange = function(event, slick, currentSlide, nextSlide) {
+        slickCarousel.updateHash(slick, currentSlide);
+        slickCarousel.changeDirection(slick, currentSlide);
+    };
+
+    slickCarousel.beforeChange = function(event, slick, currentSlide, nextSlide) {
+        slickCarousel.prevSlide = currentSlide;
+    };
+
+    slickCarousel.findInitialSlide = function(elem) {
+        var location_hash = window.location.hash;
+
+        if (location_hash != "" || location_hash != undefined) {
+            var hash = location_hash.split("#")[1];
+
+            var $slide = jQuery(jQuery(elem).find("[data-path='"+hash+"']"));
+
+            if ($slide.length) {
+                return $slide.data('index');
+            }
+        }
+        return 0;
+    };
+
+    slickCarousel.initProperties = function(initialSlide, elem) {
+        slickCarousel.initialSlide = initialSlide;
+        slickCarousel.prevSlide = initialSlide;
+        slickCarousel.elem = elem;
+    }
+
+    slickCarousel.init = function(elem) {
+        var initialSlide = slickCarousel.findInitialSlide(elem);
+        
+        slickCarousel.initProperties(initialSlide, elem);
+
+        jQuery(elem).slick({
+            infinite: true,
+            slidesToShow: 1,
+            initialSlide: initialSlide,
+            dots: false,
+            speed: 500,
+        });
+
+        jQuery(elem).on('afterChange', function(event, slick, currentSlide, nextSlide) {
+            slickCarousel.afterChange(event, slick, currentSlide, nextSlide);
+        });
+
+        jQuery(elem).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            slickCarousel.beforeChange(event, slick, currentSlide, nextSlide);
+        });
+
+        jQuery(".carousel-image-wrapper, .video-slide iframe").mouseover(function () {
+            slickCarousel.initialPosition();
+        });
+    };
+
+    jQuery(document).ready(function() {
+        slickCarousel.init(".slideshow-wrapper");
+    });
 });
